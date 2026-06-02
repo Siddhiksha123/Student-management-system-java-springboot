4ca5bb2f1e83bc263bd3e6649e4742c1de23f8dc
Swati <808284siddhiksha@gmail.com>
2026-06-02 19:51:04 +0530

    Initial commit: Added student management system with Spring Security

---
+++ src/main/java/com/student/project/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.student.project.repository;
+
+import com.student.project.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
---
+++ src/main/java/com/student/project/service/StudentService.java
@@ -0,0 +1,22 @@
+package com.student.project.service;
+
+import com.student.project.dto.AddStudentRequestDto;
+import com.student.project.dto.StudentDto;
+
+import java.util.List;
+import java.util.Map;
+
+public interface StudentService {
+
+    List<StudentDto> getAllStudents();
+
+    StudentDto getStudentById(Long id);
+
+    StudentDto createNewStudent(AddStudentRequestDto addStudentRequestDto);
+
+    void deleteStudentById(Long id);
+
+    StudentDto updateStudent(Long id, AddStudentRequestDto addStudentRequestDto);
+
+    StudentDto updatePartialStudent(Long id, Map<String, Object> updates);
+}
